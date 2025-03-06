20aef91a7e502e002eab070d995a1fa07e636c9e
Montu Meena <158208396+montumeena141@users.noreply.github.com>
2025-03-06 12:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 //your JS code here. If required.
-async function displayMessage(text, delay) {
-    await new Promise(resolve => setTimeout(resolve, delay));
-    document.getElementById('output').innerText = text;
-}
 
-// Add event listener to the button
-document.getElementById('btn').addEventListener('click', async function() {
-    let text = document.getElementById('text').value;
-    let delay = document.getElementById('delay').value;
-    await displayMessage(text, delay);
-});
+let output=document.getElementById("output");
+let text=document.getElementById("text");
+let delay=document.getElementById("delay");
+
+document.getElementById("btn").addEventListener("click",async()=>{
+	let p=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(); //output.innerText=text;
+		},delay.value)
+	})
+	let res=await p;
+	output.innerText=text.value;
+})
+
+
+// async function displayMessage(text, delay) {
+//     await new Promise(resolve => setTimeout(resolve, delay));
+//     document.getElementById('output').innerText = text;
+// }
+
+// // Add event listener to the button
+// document.getElementById('btn').addEventListener('click', async function() {
+//     let text = document.getElementById('text').value;
+//     let delay = document.getElementById('delay').value;
+//     await displayMessage(text, delay);
+// });
